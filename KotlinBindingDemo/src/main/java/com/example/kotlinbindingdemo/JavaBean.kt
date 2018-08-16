db4d580e17c1d8e05cc487d77572611308966ae6
GibsonCool <cxxceo@163.com>
2018-08-16 14:31:48 +0800

    双向绑定

--- KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/JavaBean.kt
+++ KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/JavaBean.kt
@@ -1,3 +1,7 @@
 package com.example.kotlinbindingdemo
 
+import android.databinding.ObservableField
+
 data class User(var firstName: String, var lastName: String, var age: Int)
+
+data class UserObservable(var name: ObservableField<String>, var url: ObservableField<String>)
--- KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/MainActivity.kt
+++ KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.example.kotlinbindingdemo
 
 import android.databinding.DataBindingUtil
+import android.databinding.ObservableField
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.kotlinbindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var binding: ActivityMainBinding?= null
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding?.user = User(" Gibson", "Cool", 24)
+        binding?.userOb = UserObservable(ObservableField("cxx"), ObservableField("www.baidu.com"))
+        binding?.viewSubbb?.viewStub?.inflate()
+        binding?.presenter = Presenter()
+
     }
 
-    class Presenter {
-        fun changeImage(view: View) {
+    inner class Presenter {
+        fun textChange(text: CharSequence, start: Int, lengthBefore: Int, lengthAfter: Int) {
+            Log.e("cxxinfo", text.toString())
+            var user = binding?.user
+            user?.age = text.length
+            binding?.user = user
 
+        }
+
+        fun changeImage(view: View) {
+            Toast.makeText(view.context, "change", Toast.LENGTH_SHORT).show()
+            binding?.userOb?.name?.set("name changed by buttondd")
         }
     }
 }
