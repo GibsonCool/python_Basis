864e4167d4c769a16181282ce14d3cab1a6fb8c5
GibsonCool <cxxceo@163.com>
2018-08-16 11:35:47 +0800

    second comit

--- KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/MainActivity.kt
+++ KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.kotlinbindingdemo
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.example.kotlinbindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    var binding: ActivityMainBinding?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.user = User(" Gibson", "Cool", 24)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding?.user = User(" Gibson", "Cool", 24)
+    }
+
+    class Presenter {
+        fun changeImage(view: View) {
+
+        }
     }
 }
