cbfc7be8bfa68e6a024ac47da42e5a410231c851
GibsonCool <cxxceo@163.com>
2018-08-16 16:08:26 +0800

    BindingAdapter

--- KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/JavaBean.kt
+++ KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/JavaBean.kt
@@ -1,7 +1,10 @@
 package com.example.kotlinbindingdemo
 
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 
 data class User(var firstName: String, var lastName: String, var age: Int)
 
 data class UserObservable(var name: ObservableField<String>, var url: ObservableField<String>)
+
+data class ImageInfo(var tag:ObservableBoolean , var url: ObservableField<String>)
--- KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/MainActivity.kt
+++ KotlinBindingDemo/src/main/java/com/example/kotlinbindingdemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.kotlinbindingdemo
 
+import android.databinding.BindingAdapter
 import android.databinding.DataBindingUtil
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.example.kotlinbindingdemo.databinding.ActivityMainBinding
 
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding?.user = User(" Gibson", "Cool", 24)
         binding?.userOb = UserObservable(ObservableField("cxx"), ObservableField("www.baidu.com"))
+        binding?.imgInfo = ImageInfo(ObservableBoolean(true), ObservableField("s"))
         binding?.viewSubbb?.viewStub?.inflate()
         binding?.presenter = Presenter()
 
@@ -30,9 +34,29 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        fun changeImage(view: View) {
+        fun changeObservableName(view: View) {
             Toast.makeText(view.context, "change", Toast.LENGTH_SHORT).show()
             binding?.userOb?.name?.set("name changed by buttondd")
         }
+
+        fun changeImage(info: ImageInfo) {
+            info.url.set(if (info.tag.get()) "this is true tag" else "this is FALSE tag")
+            info.tag.set(!info.tag.get())
+        }
+
     }
 }
+
+
+@BindingAdapter("app:url")
+fun changeInfoByBindingAdapter(view: TextView, info: String) {
+    Log.e("cxxinfo", "changeImgByBindingAdapter   info:$info")
+    view.text =  info
+}
+
+
+
+
+
+
+
