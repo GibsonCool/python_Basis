2abed597c98da3b411c93c0f0c26ae246eeed8e1
GibsonCool <cxxceo@163.com>
2018-08-16 18:39:56 +0800

    创建Dagger2Demo工工程

---
+++ Dagger2Demo/src/main/java/com/example/dagger2demo/javaBean/Student.java
@@ -0,0 +1,36 @@
+package com.example.dagger2demo.javaBean;
+
+import javax.inject.Inject;
+
+public class Student
+{
+	private String name;
+	private int age;
+
+	@Inject
+	public Student(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+}
